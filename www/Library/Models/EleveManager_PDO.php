997bb83b3e65114c5914d1b16bb34cde04da24e9
mik0_91 <mikael.popowicz@gmail.com>
2014-01-23 12:54:33 +0100

    Devoir

--- www/Library/Models/EleveManager_PDO.php
+++ www/Library/Models/EleveManager_PDO.php
@@ -17,7 +17,7 @@ class EleveManager_PDO extends EleveManager
 	
 	public function getUnique($id)
 	{
-		$requete = $this->dao->prepare('SELECT id_u AS id FROM user WHERE id_u = :id');
+		$requete = $this->dao->prepare('SELECT id_u AS id FROM eleve WHERE id_u = :id');
 		$requete->bindValue(':id', $id, \PDO::PARAM_INT);
 		$requete->execute();
 		$requete->setFetchMode(\PDO::FETCH_CLASS | \PDO::FETCH_PROPS_LATE, '\Library\Entities\Eleve');
@@ -34,29 +34,14 @@ class EleveManager_PDO extends EleveManager
 	
 	protected function add(Eleve $eleve)
 	{
-	    $requete = $this->dao->prepare('INSERT INTO user SET username = :username, nom = :nom, prenom = :prenom, email = :email, password = :password, salt =:salt, token = :token, dateUser = now()');
-	    $requete->bindValue(':username', $user['username']);
-		$requete->bindValue(':nom', $user['nom']);
-		$requete->bindValue(':prenom', $user['prenom']);
-		$requete->bindValue(':email', $user['email']);
-		$requete->bindValue(':password', $user['password']);
-		$requete->bindValue(':salt', $user['salt']);
-		$requete->bindValue(':token', $user['token']);
+	    $requete = $this->dao->prepare('PROCEDURE SQL');
+	    $requete->bindValue(':id', $user['id']);
 	    $requete->execute();
 	}
 	
 	protected function modify(Eleve $eleve)
 	  {
-	    $requete = $this->dao->prepare('UPDATE user SET username = :username, nom = :nom, prenom = :prenom, email = :email, password = :password, salt =:salt, token = :token, active = :active, dateUser = :dateUser WHERE id_u = :id');
-	    $requete->bindValue(':username', $user['username']);
-		$requete->bindValue(':nom', $user['nom']);
-		$requete->bindValue(':prenom', $user['prenom']);
-		$requete->bindValue(':email', $user['email']);
-		$requete->bindValue(':password', $user['password']);
-		$requete->bindValue(':salt', $user['salt']);
-		$requete->bindValue(':token', $user['token']);
-		$requete->bindValue(':active', $user['active']);
-		$requete->bindValue(':dateUser', $user['dateUser']->format('Y-m-d H:i:s'));
+	    $requete = $this->dao->prepare('PROCEDURE SQL');
 		$requete->bindValue(':id', $user['id']);
 	    $requete->execute();
 	  }
